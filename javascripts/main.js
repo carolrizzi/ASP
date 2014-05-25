d2ab0e25f4daa5747c4a26f327b2b9ebf8c9eaef
Carol Rizzi <carol.rizziray@gmail.com>
2014-05-25 08:51:50 +0100

    Finalization of ASP Website v1.0
    Addition of video and graphics of experiment 1.
    Effects of menu buttons.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,24 @@
 
-changeContent = function (pagename) {
+mouseenter = function (object) {
+	object.style.background = 'transparent url(images/menu-button-over.png) 0 0 no-repeat';
+	// object.style.background = 'transparent url(images/menu-button-highlight.png) 0 0 no-repeat';
+}
+
+mouseleave = function (object) {
+	object.style.background = 'transparent url(images/menu-button.png) 0 0 no-repeat';
+}
+
+mousedown = function (object) {
+	object.style.background = 'transparent url(images/menu-button-click.png) 0 0 no-repeat';
+}
+
+mouseup = function (object) {
+	object.style.background = 'transparent url(images/menu-button-over.png) 0 0 no-repeat';
+	// object.style.background = 'transparent url(images/menu-button-highlight.png) 0 0 no-repeat';
+}
+
+mouseclick = function (object, pagename) {
 	document.getElementById('iframe_content').src = pagename + '.html';
 }
+
+// menu-button-highlight.png
